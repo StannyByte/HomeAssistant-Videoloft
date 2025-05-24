c5b9cfb405f29f6fba416492359028484694a29e
StannyByte <stanners@duck.com>
2025-05-24 17:07:24 +0100

    Delete custom_components/videoloft/panel directory

--- custom_components/videoloft/panel/js/utils/ui.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// Utility: shorthand for document.getElementById
-const $ = (id) => document.getElementById(id);
-
-const capitalizeFirstLetter = (string) =>
-  string.charAt(0).toUpperCase() + string.slice(1); 
